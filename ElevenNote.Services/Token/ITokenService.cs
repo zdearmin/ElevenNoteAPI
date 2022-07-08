c77b76c8e53fb10776e69ecf192af19faa6c2ef6
Zach Dearmin <zacharydearmin@gmail.com>
2022-07-08 18:09:36 -0400

    Generate token added

---
+++ ElevenNote.Services/Token/ITokenService.cs
@@ -0,0 +1,10 @@
+using System.Threading.Tasks;
+using ElevenNote.Models.Token;
+
+namespace ElevenNote.Services.Token
+{
+    public interface ITokenService
+    {
+        Task<TokenResponse> GetTokenAsync(TokenRequest model);
+    }
+}
--- ElevenNote.WebAPI/Program.cs
+++ ElevenNote.WebAPI/Program.cs
@@ -1,6 +1,12 @@
+using System.Security.AccessControl;
+using System.Text;
 using ElevenNote.Data;
 using ElevenNote.Services.User;
+using ElevenNote.Services.Token;
 using Microsoft.EntityFrameworkCore;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +18,22 @@ builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlSer
 
 //Add User Service/Interface for Dependency Injection here
 builder.Services.AddScoped<IUserService, UserService>();
+// Add Token Service/Interface for Dependency Injection here
+builder.Services.AddScoped<ITokenService, TokenService>();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.RequireHttpsMetaData = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
 
 // Add services to the container.
 
