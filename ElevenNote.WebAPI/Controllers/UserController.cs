07a95ed25282d36abd4474ddb9948d3771004649
Zach Dearmin <zacharydearmin@gmail.com>
2022-07-08 14:29:45 -0400

    Authorization feature added to UserController_ GetById

--- ElevenNote.WebAPI/Controllers/UserController.cs
+++ ElevenNote.WebAPI/Controllers/UserController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ElevenNote.Services.User;
 using ElevenNote.Models.User;
+using Microsoft.AspNetCore.Authorization;
 
 namespace ElevenNote.WebAPI.Controllers
 {
@@ -37,7 +38,8 @@ namespace ElevenNote.WebAPI.Controllers
             return BadRequest("User could not be registered.");
         }
 
-        [HttpGet("{userId:int")]
+        [Authorize]
+        [HttpGet("{userId:int}")]
         public async Task<IActionResult> GetById([FromRoute] int userId)
         {
             var userDetail = await _service.GetUserByIdAsync(userId);
